89483ff71357b939bdac74c34ab19f6d3fac8a55
Haofan144 <32778232+Haofan144@users.noreply.github.com>
2018-03-11 21:06:33 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #Usage:
+#chmod 777 ./run.sh
 #./run.sh parameter1 parameter2
 #  parameter1 is the thread number for multiprocessing stage, choose from 1, 2, 4, 8
-#  parameter 2 is the population size (instruction size) for generic algorithm, choose from 160000, 40000, 10000, 2500, 625
+#  parameter 2 is the population size (instruction size) for genetic algorithm, choose from 160000, 40000, 10000, 2500, 625(or other integers)
 
 programname=$0
 
