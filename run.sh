d53bb895dbf308a1fedefc146e0cc1a9187d6dbc
Haofan144 <32778232+Haofan144@users.noreply.github.com>
2018-03-11 18:56:43 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Usage:
+#./run.sh parameter1 parameter2
+#  parameter1 is the thread number for multiprocessing stage, choose from 1, 2, 4, 8
+#  parameter 2 is the population size (instruction size) for generic algorithm, choose from 160000, 40000, 10000, 2500, 625
+
+programname=$0
+
+function usage {
+    echo "usage : $programname {population number} {thread number}"
+    exit 1
+}
+
+
+if [ $# -lt 2 ]; then
+    usage
+else
+sudo docker run --privileged deapdocker perf stat -e instructions,cycles,branch-misses,L1-dcache-load-misses,L1-icache-load-misses,LLC-load-misses,LLC-store-misses python /architecture/onemax.py "$1" "$2"
+fi 
