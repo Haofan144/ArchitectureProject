546592302e461906c60f4006147d9ee02b02ab54
Haofan144 <32778232+Haofan144@users.noreply.github.com>
2018-03-11 21:09:30 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 #Usage:
+#chmod 777 ./setup.sh
 #./setup/sh
 
-#get this computer's kernel version 
+#get local computer's kernel version 
 kernel_version='uname -r'
 echo "The kernel version of this computer is: ${kernel_version}"
 
-#Download Dockerfile and onemax.py from my github 
+#Download Dockerfile and onemax.py from my github https://github.com/Haofan144/ArchitectureProject.git
 rm arch_project -fr
 mkdir arch_project
 cd arch_project
 git clone https://github.com/Haofan144/ArchitectureProject.git
 
-#adjust kernel version according to the environment for docker 
+#adjust kernel version for docker 
 cd ArchitectureProject
 sed -i "s/KERNEL_VERSION/${kernel_version}/g" Dockerfile
 
